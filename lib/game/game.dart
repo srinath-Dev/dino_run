0ba1d7859f9c62208fd5963281ee1a87a801e8e1
SrinathDev <nathsri258@gmail.com>
2021-10-10 13:28:17 +0530

    game resource setup

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,18 +1,42 @@
 import 'package:flame/components/animation_component.dart';
+import 'package:flame/components/parallax_component.dart';
 import 'package:flame/game.dart';
 import 'package:flame/spritesheet.dart';
 
 class DinoGame extends BaseGame {
   late AnimationComponent _dino;
+  late ParallaxComponent _parallaxComponent;
 
   DinoGame() {
     _dino = AnimationComponent.empty();
-    SpriteSheet(imageName: 'DinoSprites - tard.png',
-        textureWidth: null,
-        textureHeight: null,
-        columns: null,
-        rows: null);
+    final spriteSheet = SpriteSheet(
+        imageName: 'DinoSprites - tard.png',
+        textureWidth: 24,
+        textureHeight: 24,
+        columns: 24,
+        rows: 1);
+
+    final idleAnimation = spriteSheet.createAnimation(0, from: 0, to: 3, stepTime: 0.1);
+    final runAnimation = spriteSheet.createAnimation(0, from: 4, to: 10, stepTime: 0.1);
+    _dino.animation = runAnimation;
+    _dino.width = 80;
+    _dino.height = 80;
+
+    _dino.x = 80;
+    _dino.y = 200;
+
+
+    _parallaxComponent = ParallaxComponent(
+      [
+        ParallaxImage('parallax/plx-1.png'),
+        ParallaxImage('parallax/plx-2.png'),
+        ParallaxImage('parallax/plx-3.png'),
+        ParallaxImage('parallax/plx-4.png'),
+        ParallaxImage('parallax/plx-5.png'),
+        ParallaxImage('parallax/plx-6.png',fill: LayerFill.none),
+      ]
+    );
+    add(_parallaxComponent);
     add(_dino);
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flame/components/component.dart';
+import 'package:DinoWarrior/game/game.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/game/base_game.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -30,21 +29,18 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late BaseGame game;
+  late DinoGame game;
 
   @override
   void initState() {
     super.initState();
-    game = BaseGame();
-    var dinoSprite = SpriteComponent.square(64, 'DinoSprites_tard.gif');
-    dinoSprite.x = 100;
-    dinoSprite.y = 100;
-    game.add(dinoSprite);
+    game = DinoGame();
   }
 
   @override
