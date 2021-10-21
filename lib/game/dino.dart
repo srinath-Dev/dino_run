93d45a988303413320b3470e84da6302a731503b
SrinathDev <nathsri258@gmail.com>
2021-10-21 15:02:08 +0530

    Dino leg movement

---
+++ lib/game/dino.dart
@@ -0,0 +1,76 @@
+import 'dart:ui';
+
+import 'package:flame/animation.dart';
+import 'package:flame/components/animation_component.dart';
+import 'package:flame/spritesheet.dart';
+
+const double groundHeight = 32;
+const double dinoTopBottomSpacing = 10;
+const int numberOfTilesAlongWidth = 10;
+const double Gravity = 1000;
+
+class Dino extends AnimationComponent {
+  late Animation _runAnimation;
+  late Animation _hitAnimation;
+
+  double speedY = 0.0;
+  double yMax = 0.0;
+
+  Dino() : super.empty() {
+    final spriteSheet = SpriteSheet(
+        imageName: 'DinoSprites - tard.png',
+        textureWidth: 24,
+        textureHeight: 24,
+        columns: 24,
+        rows: 1);
+
+    _runAnimation =
+        spriteSheet.createAnimation(0, from: 4, to: 10, stepTime: 0.1);
+    _hitAnimation =
+        spriteSheet.createAnimation(0, from: 14, to: 16, stepTime: 0.1);
+    this.animation = _runAnimation;
+  }
+
+  @override
+  void update(double t) {
+    super.update(t);
+    //final velocity  = initial velocity + gravity * time;
+    // v = u + at;
+    this.speedY += Gravity * t;
+
+    //d =  s0 + s*t
+    this.y += this.speedY * t;
+
+    if(isOnGround()){
+      this.y = this.yMax;
+      this.speedY = 0.0;
+    }
+  }
+
+  bool isOnGround(){
+    return (this.y >= this.yMax);
+  }
+
+  @override
+  void resize(Size size) {
+    super.resize(size);
+    this.height = this.width = size.width / numberOfTilesAlongWidth;
+    this.x = this.width;
+    this.y = size.height - groundHeight - this.height + dinoTopBottomSpacing;
+    this.yMax = this.y;
+  }
+
+  void run() {
+    this.animation = _runAnimation;
+  }
+
+  void hit() {
+    this.animation = _hitAnimation;
+  }
+
+  void jump() {
+    if(isOnGround()){
+      this.speedY = -600;
+    }
+  }
+}
--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,38 +1,15 @@
-import 'package:flame/components/animation_component.dart';
+import 'package:DinoWarrior/game/dino.dart';
 import 'package:flame/components/parallax_component.dart';
 import 'package:flame/game.dart';
-import 'package:flame/spritesheet.dart';
+import 'package:flame/gestures.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 
-const double groundHeight = 32;
-const double dinoTopBottomSpacing = 10;
-const int numberOfTilesAlongWidth = 10;
-
-class DinoGame extends BaseGame {
-  late AnimationComponent _dino;
+class DinoGame extends BaseGame with TapDetector{
+  late Dino _dino;
   late ParallaxComponent _parallaxComponent;
 
   DinoGame() {
-    _dino = AnimationComponent.empty();
-    final spriteSheet = SpriteSheet(
-        imageName: 'DinoSprites - tard.png',
-        textureWidth: 24,
-        textureHeight: 24,
-        columns: 24,
-        rows: 1);
-
-    final idleAnimation =
-        spriteSheet.createAnimation(0, from: 0, to: 3, stepTime: 0.1);
-    final runAnimation =
-        spriteSheet.createAnimation(0, from: 4, to: 10, stepTime: 0.1);
-    _dino.animation = runAnimation;
-    _dino.width = 80;
-    _dino.height = 80;
-
-    _dino.x = 80;
-    _dino.y = 200;
-
     _parallaxComponent = ParallaxComponent(
       [
         ParallaxImage('parallax/plx-1.png'),
@@ -46,14 +23,13 @@ class DinoGame extends BaseGame {
       layerDelta: Offset(20, 0),
     );
     add(_parallaxComponent);
+    _dino = Dino();
     add(_dino);
   }
 
   @override
-  void resize(Size size) {
-    super.resize(size);
-    _dino.height = _dino.width = size.width / numberOfTilesAlongWidth;
-    _dino.x = _dino.width;
-    _dino.y = size.height - groundHeight - _dino.height + dinoTopBottomSpacing;
+  void onTapDown(TapDownDetails details) {
+    super.onTapDown(details);
+    _dino.jump();
   }
 }
